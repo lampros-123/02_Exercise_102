7bc38312f54aa4d74bdb7e5e34ccb09f4bf5a036
lampros <matthias@futurefactory-software.com>
2018-09-25 09:40:24 +0200

    create GUI, Model and Datei classes

---
+++ src/Datei.java
@@ -0,0 +1,15 @@
+
+import java.io.File;
+
+
+/**
+ *
+ * @author Matthias
+ */
+public class Datei extends File {
+    
+    public Datei(String pathname) {
+        super(pathname);
+    }
+    
+}
---
+++ src/DateiModel.java
@@ -0,0 +1,21 @@
+
+import javax.swing.AbstractListModel;
+
+
+/**
+ *
+ * @author Matthias
+ */
+public class DateiModel extends AbstractListModel {
+
+    @Override
+    public int getSize() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object getElementAt(int index) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
